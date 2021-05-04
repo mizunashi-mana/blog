9d9b216d0a3a297e2e76e7fb406097bdbe17cabb
Mizunashi Mana <mizunashi-mana@noreply.git>
2021-05-05 00:01:27 +0900

    Update node dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,82 +2,83 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'production',
+    mode: 'production',
 
-  entry: {
-    bundle: './frontend/index.js',
-  },
+    entry: {
+        bundle: './frontend/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'content/dist-asset')
+    },
 
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'content/dist-asset')
-  },
-
-  resolve: {
-    alias: {
-      vue$: 'vue/dist/vue.esm.js',
-      katex$: 'katex/dist/katex.mjs',
-      katex: 'katex',
-    }
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
+    resolve: {
+        alias: {
+            vue$: 'vue/dist/vue.esm.js',
+            katex$: 'katex/dist/katex.mjs',
+            katex: 'katex',
         }
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('cssnano')({
-                    preset: ['default', {
-                      mergeRules: false,
-                    }]
-                  }),
-                  require('precss'),
-                  require('autoprefixer')
-                ];
-              }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: function () {
+                                    return [
+                                        require('cssnano')({
+                                            preset: ['default', {
+                                                mergeRules: false,
+                                            }]
+                                        }),
+                                        require('precss'),
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(ttf|woff2?)$/,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    },
+                ]
             }
-          },
-        ]
-      },
-      {
-        test: /\.(ttf|woff2?)$/,
-        use: [
-          {
-            loader: 'file-loader'
-          },
-        ]
-      }
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
     ],
-  },
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
-  ],
-
-  performance: {
-    maxEntrypointSize: 1024 * 1024,
-    maxAssetSize: 1024 * 1024,
-  },
+    performance: {
+        maxEntrypointSize: 1024 * 1024,
+        maxAssetSize: 1024 * 1024,
+    },
 };
