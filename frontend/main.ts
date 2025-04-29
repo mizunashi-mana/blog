92891504c80b357d1736e4653df6afefe842b8ce
Mizunashi Mana <contact@mizunashi.work>
2025-04-29 23:36:23 +0900

    Improve pagespeed insights

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,34 +1,39 @@
-import './util/polyfills';
 import './lib/fontawesome';
 import './lib/katex';
 import './component/sentry_monitor';
-import * as addFootnoteTooltip from './component/add_footnote_tooltip';
-
-function onLoad(): void {
-    void addFootnoteTooltip.addFootnoteTooltip();
-}
 
 /*!
  * @license MIT https://github.com/jquery/jquery
  * Copyright JS Foundation and other contributors, https://js.foundation/
  */
-/**
- * ref: https://github.com/jquery/jquery/blob/3.4.0/src/core/ready.js#L60
- */
-function completed() {
-    document.removeEventListener("DOMContentLoaded", completed);
-    window.removeEventListener("load", completed);
-    onLoad();
+async function main(): Promise<void> {
+    await import('./util/polyfills');
+
+    const addFootnoteTooltip = import('./component/add_footnote_tooltip');
+    const onLoad = () => {
+        void (async () => await (await addFootnoteTooltip).addFootnoteTooltip())();
+    }
+
+    /**
+     * ref: https://github.com/jquery/jquery/blob/3.4.0/src/core/ready.js#L60
+     */
+    const completed = () => {
+        document.removeEventListener("DOMContentLoaded", completed);
+        window.removeEventListener("load", completed);
+        void onLoad();
+    }
+
+    if (
+        document.readyState === "complete"
+        || document.readyState !== "loading" && !("doScroll" in document.documentElement)
+    ) {
+        window.setTimeout(onLoad);
+    } else {
+        document.addEventListener("DOMContentLoaded", completed);
+
+        // A fallback to window.onload, that will always work
+        window.addEventListener("load", completed);
+    }
 }
 
-if (
-    document.readyState === "complete"
-    || document.readyState !== "loading" && !("doScroll" in document.documentElement)
-) {
-    window.setTimeout(onLoad);
-} else {
-    document.addEventListener("DOMContentLoaded", completed);
-
-    // A fallback to window.onload, that will always work
-    window.addEventListener("load", completed);
-}
+void main();
